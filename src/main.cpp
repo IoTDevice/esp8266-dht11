f0a4d092c43caee87e106ce3e66529cf1f6c5504
Farry <newfarry@126.com>
2019-08-25 22:04:13 +0800

    init

---
+++ src/main.cpp
@@ -0,0 +1,149 @@
+// DHT Temperature & Humidity Sensor
+// Unified Sensor Library Example
+// Written by Tony DiCola for Adafruit Industries
+// Released under an MIT license.
+
+// REQUIRES the following Arduino libraries:
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define DHTPIN 2     // Digital pin connected to the DHT sensor 
+// Feather HUZZAH ESP8266 note: use pins 3, 4, 5, 12, 13 or 14 --
+// Pin 15 can work but DHT must be disconnected during program upload.
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+// #define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+// See guide for details on sensor wiring and usage:
+//   https://learn.adafruit.com/dht/overview
+DHT_Unified dht(DHTPIN, DHTTYPE);
+uint32_t delayMS;
+
+const int httpPort = 80;
+String deviceName = "温湿度传感器";
+String version = "1.0";
+ESP8266WebServer server(httpPort);
+
+// web服务器的根目录
+void handleRoot() {
+  server.send(200, "text/html", "<h1>this is index page from esp8266!</h1>");
+}
+// 设备改名的API
+void handleDeviceRename(){
+  String message = "{\"code\":0,\"message\":\"success\"}";
+  for (uint8_t i=0; i<server.args(); i++){
+    if (server.argName(i)=="name")
+    {
+      deviceName = server.arg(i);
+    }
+  }
+  server.send(200, "application/json", message);
+}
+
+// 获取status
+void handleCurrentStatus(){
+  String message;
+  String temp;
+  String humi;
+    // Get temperature event and print its value.
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    temp = String(0);
+  }
+  else {
+    temp = String(event.temperature);
+  }
+  // Get humidity event and print its value.
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    humi = String(0);
+  }
+  else {
+    humi = String(event.relative_humidity);
+  }
+  message = "{\"temperature\":"+temp+
+  ",\"humidity\":"+humi+
+  ",\"code\":0,\"message\":\"success\"}";
+  server.send(200, "application/json", message);
+}
+
+// 设备信息
+void handleDeviceInfo(){
+  String message;
+  message = "{\n";
+  message += "\"name\":\""+deviceName +"\",\n";
+  message += "\"model\":\"com.iotserv.devices.dht11\",\n";
+  message += "\"mac\":\""+WiFi.macAddress()+"\",\n";
+  message += "\"id\":\""+String(ESP.getFlashChipId())+"\",\n";
+  message += "\"ui-support\":[\"web\",\"native\"],\n";
+  message += "\"ui-first\":\"native\",\n";
+  message += "\"author\":\"Farry\",\n";
+  message += "\"email\":\"newfarry@126.com\",\n";
+  message += "\"home-page\":\"https://github.com/iotdevice\",\n";
+  message += "\"firmware-respository\":\"https://github.com/iotdevice/esp8266-dht11\",\n";
+  message += "\"firmware-version\":\""+version+"\"\n";
+  message +="}";
+  server.send(200, "application/json", message);
+}
+
+// 页面或者api没有找到
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+
+void setup() {
+  WiFi.mode(WIFI_STA);
+  // 选取一种连接路由器的方式 
+  // WiFi.begin(ssid, password);
+  WiFi.beginSmartConfig();
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    // Serial.print(".");
+  }
+
+  if (MDNS.begin("esp8266")) {
+    // Serial.println("MDNS responder started");
+  }
+
+  server.on("/", handleRoot);
+  server.on("/rename", handleDeviceRename);
+  server.on("/status", handleCurrentStatus);
+  // about this device
+  server.on("/info", handleDeviceInfo);
+  // Initialize device.
+  dht.begin();
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  // Serial.println("HTTP server started");
+  MDNS.addService("iotdevice", "tcp", httpPort);
+}
+
+void loop() {
+  MDNS.update();
+  server.handleClient();
+}
